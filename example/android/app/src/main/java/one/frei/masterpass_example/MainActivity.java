606d13ccc64acf91dbb52b140ef3c27c9e2aa694
denzil-dotxml <denzil@dotxmltech.com>
2023-05-29 17:09:11 +0200

    Masterpass Plugin - v2 Embedding Updates: Updated:  - example/.../MainActivity.jave:     - Added import
     - pubspec.yaml:
        - Set flutter_embedding_version = 2

--- example/android/app/src/main/java/one/frei/masterpass_example/MainActivity.java
+++ example/android/app/src/main/java/one/frei/masterpass_example/MainActivity.java
@@ -2,6 +2,7 @@ package one.frei.masterpass_example;
 
 import android.os.Bundle;
 
+import androidx.test.rule.ActivityTestRule;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
